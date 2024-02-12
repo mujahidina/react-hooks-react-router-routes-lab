8e5d3c61616d6b23255af2a9dfbabb95a68d6022
mujahidina <mujahidabdi5070@gmail.com>
2024-02-12 15:40:14 +0300

    completed assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+   <div className="directors">
+ <h1>Directors Page</h1>
+  {directors.map((director) => (
+    <div key={director.id}>
+    <h2>Name: {director.name}</h2>
+    <ul>
+      Movies:
+      {director.movies.map((movie, index) => (
+       <li key={index}>{movie}</li>
+      ))}
+    </ul>
+    </div>
+  ))}
+
+   </div>
+  );
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="movies">
+      <h1>Movies Page</h1>
+
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <h2>Name: {movie.title}</h2>
+          <p>Time: {movie.time}</p>
+          <ul>
+            Genre:
+            {movie.genres.map((genre, index) => (
+              <li key={index}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+        
+      ))}
+    </div>
+  )
 }
 
 export default Movies;
